ad9a37a559a4d7c6bfdd9f42b66d137eb35f21e4
pongsapak598 <pongsapak.roc@gmail.com>
2023-04-09 22:39:11 +0700

    Finish actions+animations after the button was clicked

--- script.js
+++ script.js
@@ -1,41 +1,53 @@
 let inputtedTopics = ["บทวิเคราะห์ราคา", "ข่าวในประเทศ 🇹🇭", "ราคา KUB", "ข่าว Bitcoin", "ข่าว Ethereum", "เทคโนโลยี", "ข่าวสหรัฐฯ 🇺🇸", "AI", "ข่าว DeFi", "ข่าว NFT", "บทวิเคราะห์ราคา", "ข่าวในประเทศ 🇹🇭", "ราคา KUB", "ข่าว Bitcoin", "ข่าว Ethereum", "เทคโนโลยี", "ข่าวสหรัฐฯ 🇺🇸", "AI", "ข่าว DeFi", "ข่าว NFT"];
 
-function addStyle(topicBlock) {
-    const textWidth = topicBlock.textContent.length * 7 + 35;
-    topicBlock.style.width = textWidth + "px";
-    topicBlock.style.height = "39px";
-    topicBlock.style.background = "white";
-    topicBlock.style.textAlign = "center";
-    topicBlock.style.paddingTop = "2px";
-    topicBlock.style.borderRadius = "0.45em";
+function addBoxStyle(topicBox) {
+    const textWidth = topicBox.textContent.length * 7 + 35;
+    topicBox.style.width = textWidth + "px";
+    topicBox.style.height = "39px";
+    topicBox.style.background = "white";
+    topicBox.style.textAlign = "center";
+    topicBox.style.paddingTop = "2px";
+    topicBox.style.borderRadius = "0.45em";
+    topicBox.style.transition = "opacity 1s";
 }
 
-function addBox(topicBlock) {
+function addBox(topicBox) {
     const selectedTopic = document.querySelector("#poll-body");
-    selectedTopic.appendChild(topicBlock);
+    topicBox.setAttribute("onclick", "removeBox()");
+    selectedTopic.appendChild(topicBox);
 }
+
 
 function pollProcess() {
     for (i = 0 ; i < inputtedTopics.length ; i++) {
-        let topicBlock = document.createElement("button");
-        topicBlock.textContent = inputtedTopics[i];
-        addStyle(topicBlock);
-        addBox(topicBlock);
+        let topicBox = document.createElement("button");
+        topicBox.textContent = inputtedTopics[i];
+        addBoxStyle(topicBox);
+        addBox(topicBox);
     }
 }
 pollProcess();
-    
+
 function removeBox() {
 
+    const pollStyle = window.getComputedStyle(document.querySelector(".poll"));
+    const pollHeight = parseFloat(pollStyle.height);
+    
+    const pollBox = document.querySelector(".poll");
+    const pollBody = document.querySelector("#poll-body");
+    const pollHead = document.querySelector("#poll-head");
+
+    pollBody.disabled = 0;
+    pollBody.style.opacity = 0;
+
+    pollHead.disabled = 0;
+    pollHead.style.opacity = 0;
+
+    pollHead.addEventListener("transitionend", function() {
+      pollBody.remove();
+      pollHead.remove();
+    }, {once: true});
+
+    pollBox.style.height = pollHeight + "px";
+
 }
-
-/*
-
-const pollBody = document.getElementById('poll-body');
-
-pollBody.addEventListener('click', function(event) {
-  const box = event.target;
-  box.style.display = 'none';
-});
-
-*/
