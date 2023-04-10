169c544099ed92ea53012c406d82fa91ca31f3f2
pongsapak598 <pongsapak.roc@gmail.com>
2023-04-10 18:06:40 +0700

    Add responsiveness

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let inputtedTopics = ["บทวิเคราะห์ราคา", "ข่าวในประเทศ 🇹🇭", "ราคา KUB", "ข่าว Bitcoin", "ข่าว Ethereum", "ข่าวเทคโนโลยี", "ข่าวสหรัฐฯ 🇺🇸", "ข่าว AI", "ข่าว DeFi", "ข่าว NFT", "บทวิเคราะห์ราคา", "ข่าวในประเทศ 🇹🇭", "ราคา KUB", "ข่าว Bitcoin", "ข่าว Ethereum", "เทคโนโลยี", "ข่าวสหรัฐฯ 🇺🇸", "AI", "ข่าว DeFi", "ข่าว NFT"];
+const inputtedTopics = ["บทวิเคราะห์ราคา", "ข่าวในประเทศ 🇹🇭", "ราคา KUB", "ข่าว Bitcoin", "ข่าว Ethereum", "ข่าวเทคโนโลยี", "ข่าวสหรัฐฯ 🇺🇸", "ข่าว AI", "ข่าว DeFi", "ข่าว NFT", "บทวิเคราะห์ราคา", "ข่าวในประเทศ 🇹🇭", "ราคา KUB", "ข่าว Bitcoin", "ข่าว Ethereum", "เทคโนโลยี", "ข่าวสหรัฐฯ 🇺🇸", "AI", "ข่าว DeFi", "ข่าว NFT"];
 
 function addBoxStyle(topicBox) {
     const textWidth = topicBox.textContent.length * 7 + 35;
@@ -8,7 +8,7 @@ function addBoxStyle(topicBox) {
     topicBox.style.textAlign = "center";
     topicBox.style.paddingTop = "2px";
     topicBox.style.borderRadius = "0.45em";
-    topicBox.style.transition = "opacity 1s";
+    topicBox.style.transition = "opacity 0.5s";
 }
 
 function addBox(topicBox) {
@@ -18,12 +18,23 @@ function addBox(topicBox) {
 }
 
 function pollProcess() {
+
+    if (document.documentElement.clientWidth < 640) {
+        for (i = 0 ; i < 8 ; i++) {
+            let topicBox = document.createElement("button");
+            topicBox.textContent = inputtedTopics[i];
+            topicBox.value = inputtedTopics[i];
+            addBoxStyle(topicBox);
+            addBox(topicBox);
+        }
+    } else {
     for (i = 0 ; i < inputtedTopics.length ; i++) {
         let topicBox = document.createElement("button");
         topicBox.textContent = inputtedTopics[i];
         topicBox.value = inputtedTopics[i];
         addBoxStyle(topicBox);
         addBox(topicBox);
+        }
     }
 }
 pollProcess();
@@ -34,16 +45,12 @@ function boxClicked(value) {
  
     function removeBox() {
         console.log(value);
-
         const pollStyle = window.getComputedStyle(document.querySelector(".poll"));
         const pollHeight = parseFloat(pollStyle.height);
-        
         const pollBody = document.querySelector("#poll-body");
         const pollHead = document.querySelector("#poll-head");
-
         pollBody.disabled = 0;
         pollBody.style.opacity = 0;
-
         pollHead.disabled = 0;
         pollHead.style.opacity = 0;
 
@@ -54,29 +61,20 @@ function boxClicked(value) {
         }, {once: true});
 
     function showThanksText() {
-
         const poll = document.querySelector(".poll");
-
         poll.style.display = "flex";
         poll.style.justifyContent = "center";
         poll.style.alignItems = "center";
-
-
-        let thanksText = document.createElement("p");
-        thanksText.innerHTML = `ขอบคุณ! เราจะนำเสนอ <b>${value}</b> ให้มากขึ้น`;
-        thanksText.style.fontSize = "1.9em";
+        const thanksText = document.createElement("p");
+        thanksText.innerHTML = `ขอบคุณ! เราจะนำเสนอ <b>${value}</b> ให้มากขึ้น🎉`;
+        thanksText.style.fontSize = "1.5em";
         thanksText.style.transition = "opacity 1.5s";
         thanksText.style.opacity = "0";
-
-        
         poll.appendChild(thanksText);
-        
         void thanksText.offsetWidth;
         thanksText.style.opacity = "1";
       }
-
         pollBox.style.height = pollHeight + "px";
     }
-
     removeBox();
 }
